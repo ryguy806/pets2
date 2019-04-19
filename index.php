7fc315ef27e9d505424711c831d5b9b418e151ad
Chris Holtz <choltz3@mail.greenriver.edu>
2019-04-19 16:26:38 -0400

    Added a parameterized route for animals which displays the sound that the animal makes

--- index.php
+++ index.php
@@ -27,5 +27,47 @@ $f3->route('GET /', function () {
     echo '<a href="order">Order a Pet</a>';
 });
 
+//animal route
+$f3->route('GET /@animal', function ($f3, $params) {
+
+    $animal = $params['animal'];
+
+    $animalTypes = array('dog', 'cat',
+        'fish', 'bird', 'monkey');
+
+    if (!in_array($animal, $animalTypes)) {
+        echo "$animal doesnt exist";
+    }
+    else
+        {
+
+        switch ($animal)
+        {
+            case 'dog':
+                echo "<h3>Woof</h3>";
+                break;
+            case 'cat':
+                echo "<h3>Meow</h3>";
+                break;
+            case 'fish':
+                echo "<h3>Blub</h3>";
+                break;
+            case 'bird':
+                echo "<h3>Chirp</h3>";
+                break;
+            case 'monkey':
+                echo "<h3>Poo Fling</h3>";
+                break;
+            default:
+                $f3->error(404);
+
+        }
+    }
+
+
+});
+
+
+
 //Run fat-free
 $f3->run();
