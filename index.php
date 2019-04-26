d3bd59bfb76ebe838629ff5fcd2cddfe8f481970
Michael Britt <mbritt3@greenriver.mail.edu>
2019-04-26 17:23:01 -0400

    Checked if form 2 has an error, added sticky error messages

--- index.php
+++ index.php
@@ -29,20 +29,12 @@ $f3->route('GET /', function ($f3) {
 
     session_destroy();
 
-    if(isset($_POST['animalName']) && validString($_POST['animalName'])){
-        $_SESSION['animalName'] = $_POST['animalName'];
-        $f3->reroute('/order2');
-    } else {
-        $f3->set("errors['animal']", "Please enter an animal.");
-    }
-
-
     echo '<h1>My Pets</h1>';
     echo '<a href="order">Order a Pet</a>';
 });
 
 //animal route
-$f3->route('GET | POST /@animal', function ($f3, $params) {
+$f3->route('GET | POST/animal', function ($f3, $params) {
 
     $animal = $params['animal'];
 
@@ -81,23 +73,37 @@ $f3->route('GET | POST /@animal', function ($f3, $params) {
 
 });
 
-$f3->route('GET | POST /order', function () {
+$f3->route('GET|POST /order', function ($f3) {
 
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if(isset($_POST['animalName']) && validString($_POST['animalName'])){
+            $_SESSION['animalName'] = $_POST['animalName'];
+            $f3->reroute('/order2');
+        } else {
+            $f3->set("errors['animal']", "Please enter an animal.");
+        }
+    }
     $view = new Template();
     echo $view->render('views/form1.html');
 });
 
-$f3->route('GET | POST /order2', function () {
+$f3->route('GET|POST /order2', function ($f3) {
 
-    $_SESSION['animalName'] = $_POST['animalName'];
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if(isset($_POST['color']) && validColor($_POST['color'])){
+            $_SESSION['color'] = $_POST['color'];
+            $f3->reroute('/results');
+        } else {
+            $f3->set("errors['color']", "Please enter an animal.");
+        }
+    }
     $view = new Template();
     echo $view->render('views/form2.html');
 });
 
-$f3->route('POST /results', function () {
-
-    $_SESSION['color'] = $_POST['color'];
+$f3->route('GET|POST /results', function () {
 
     $view = new Template();
     echo $view->render('views/results.html');
